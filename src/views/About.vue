eae4ef55953788600c51bb5ac12a54ed066e6f98
ricci <ricci@anwin-tech.com>
2019-04-17 11:54:41 +0800

    环境搭建

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <span>1112313</span>
   </div>
 </template>
+
+<script>
+export default {
+  mounted() {
+    console.log(11111);
+  }
+};
+</script>
+
+<style lang="stylus">
+.about
+  background divcolor
+  color color1
+  span
+    display block
+    width 100px
+    height 100px
+    background #000
+    color color1
+</style>
---
+++ vue.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const CompressionPlugin = require("compression-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+module.exports = {
+  configureWebpack: {
+    optimization: {
+      minimizer: [
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            compress: {
+              warnings: false,
+              drop_console: true, //consoledrop_debugger:false,
+              pure_funcs: ["console.log"] //移除console
+            }
+          }
+        })
+      ]
+    }
+  },
+  chainWebpack: config => {
+    if (process.env.NODE_ENV === "production") {
+      config
+        .plugin("compression")
+        .use(CompressionPlugin, {
+          asset: "[path].gz[query]",
+          algorithm: "gzip",
+          test: new RegExp("\\.(" + ["js", "css"].join("|") + ")$"),
+          threshold: 10240,
+          minRatio: 0.8,
+          cache: true
+        })
+        .tap(args => {});
+    }
+    const types = ["vue-modules", "vue", "normal-modules", "normal"];
+    types.forEach(type =>
+      addStyleResource(config.module.rule("stylus").oneOf(type))
+    );
+  }
+};
+
+function addStyleResource(rule) {
+  rule
+    .use("style-resource")
+    .loader("style-resources-loader")
+    .options({
+      patterns: [path.resolve(__dirname, "./src/styles/imports.styl")]
+    });
+}
